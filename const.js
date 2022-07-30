0b7775f06a13ad5edf5fafd49a0dca3f967107a4
Alekszac <aleks.zac@gmail.com>
2022-07-30 12:30:16 +0300

    First commit

---
+++ const.js
@@ -0,0 +1,17 @@
+const commands = `
+/start - Перезапустить бота
+/help - Помощь
+`
+const text1 = `
+1. Текст для проверки оброботчика <a href="http://site.ru">указатель ссылки</a>
+`
+const text2 = `
+2. Текст для проверки оброботчика
+`
+const text3 = `
+3. Текст для проверки оброботчика
+`
+module.exports.commands = commands
+module.exports.text1 = text1
+module.exports.text2 = text2
+module.exports.text3 = text3
